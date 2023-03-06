324b0472ad455f6a0b31453765171246bb6bcf3b
Yogesh K <armsvcmode@gmail.com>
2023-03-06 11:22:24 +0530

    Introduce HTML templating and inheritance

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -10,6 +10,8 @@ import (
     "fmt"
     "strconv"
     "net/http"
+    "html/template"
+    "log"
 )
 
 func home(w http.ResponseWriter, r *http.Request){
@@ -21,7 +23,31 @@ func home(w http.ResponseWriter, r *http.Request){
         http.NotFound(w, r)
         return
     }
-    w.Write([]byte("Hello from Chunkbox"))
+    // Initialize a slice containing the paths to the two files. It's important
+    // to note that the file containing our base template must be the *first*
+    // file in the slice.
+    files := []string{
+        "./ui/html/base.html",
+        "./ui/html/pages/home.html",
+    }
+    // Use the template.ParseFiles() function to read the template file into a
+    // template set. If there's an error, we log the detailed error message and use
+    // the http.Error() function to send a generic 500 Internal Server Error
+    // response to the user.
+    ts, err := template.ParseFiles(files...)
+    if err != nil {
+        log.Print(err.Error())
+        http.Error(w, "Internal Server Error", 500)
+        return
+    }
+
+    // Use the ExecuteTemplate() method to write the content of the "base" 
+    // template as the response body.
+    err = ts.ExecuteTemplate(w, "base", nil)
+    if err != nil {
+        log.Print(err.Error())
+        http.Error(w, "Internal Server Error", 500)
+    }
 }
 
 func chunkboxView(w http.ResponseWriter, r *http.Request){
