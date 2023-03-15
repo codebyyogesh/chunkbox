f29cb956c3d0157b2a2677183a71319838c02da1
Yogesh K <armsvcmode@gmail.com>
2023-03-15 16:05:00 +0530

    add mysql support and initialize it

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -10,10 +10,12 @@
 package main
 
 import (
+    "database/sql"
     "log"
     "net/http"
     "flag"
     "os"
+    _ "github.com/go-sql-driver/mysql" //we need the driver’s init() function to run so that it can register itself with the database/sql package.
 )
 
 // Define an application struct to hold the application-wide dependencies for the
@@ -38,6 +40,8 @@ func main() {
     // and some short help text explaining what the flag controls. The value of the
     // flag will be stored in the addr variable at runtime.
     addr := flag.String("addr", ":3001", "HTTP network address")
+    // Define a new command-line flag for the MySQL DSN string.
+    dsn := flag.String("dsn", "web:pass@/chunkbox?parseTime=true", "MySQL data source name")
     // Importantly, we use the flag.Parse() function to parse the command-line flag.
     // This reads in the command-line flag value and assigns it to the addr
     // variable. You need to call this *before* you use the addr variable
@@ -57,6 +61,16 @@ func main() {
     // file name and line number.
     errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
+    // We pass openDB() the DSN from the command-line flag.
+    db, err := openDB(*dsn)
+    if err != nil {
+        errorLog.Fatal(err)
+    }
+    // We also defer a call to db.Close(), so that the connection pool is closed
+    // before the main() or program exits. It actually will never run
+    // in this scenario because of errorLog.Fatal() which terminates
+    // the program immediately.
+    defer db.Close()
     // Initialize a new instance of our application struct, containing the
     // dependencies.
     app := &application{
@@ -82,8 +96,22 @@ func main() {
 
     // Instead of the default http.ListenAndServe(), we will use the newly created
     // http server struct. Call the ListenAndServe() method on our new http.Server struct. 
-    err := srv.ListenAndServe()
+    // err is already declared above.
+    err = srv.ListenAndServe()
     errorLog.Fatal(err)
 }
 
 
+// The openDB() function wraps sql.Open() and returns a sql.DB connection pool
+// for a given DSN.
+func openDB(dsn string) (*sql.DB, error) {
+    db, err := sql.Open("mysql", dsn)
+    if err != nil {
+        return nil, err
+    }
+    //create a connection and check for any errors.
+    if err = db.Ping(); err != nil {
+        return nil, err
+    }
+    return db, nil
+}
