7045e4d4f4ea677e0c8aad7ffcb72c83508a1ad2
Yogesh K <yogeshcodes@zohomail.in>
2023-05-11 11:52:07 +0530

    Create custom template functions (human date display format changed).

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -8,6 +8,7 @@ package main
 import(
     "html/template"
     "path/filepath"
+    "time"
     "github.com/cpucortexm/chunkbox/internal/models"
  )
 // Define a templateData type to act as the holding structure for
@@ -16,6 +17,19 @@ type templateData struct {
     CurrentYear int
     Chunk *models.Chunk
     Chunks []*models.Chunk // Chunks field for holding a slice of chunks
+}
+
+// Create a humanDate function which returns a nicely formatted string
+// representation of a time.Time object.
+func humanDate(t time.Time) string {
+    return t.Format("02 Jan 2006 at 15:04")
+}
+
+// Initialize a template.FuncMap object and store it in a global variable. This is
+// essentially a string-keyed map which acts as a lookup between the names of 
+// custom template functions and the functions themselves.
+var functions = template.FuncMap{
+    "humanDate": humanDate,
 }
 
 func newTemplateCache() (map[string]*template.Template, error){
@@ -36,9 +50,11 @@ func newTemplateCache() (map[string]*template.Template, error){
         // Extract the file name (like 'home.html') from the full filepath
         // and assign it to the name variable.
         name := filepath.Base(page)
-
-        // Parse the base template file into a template set.
-        ts, err := template.ParseFiles("./ui/html/base.html")
+        // The template.FuncMap must be registered with the template set before you
+        // call the ParseFiles() method. This means we have to use template.New() to
+        // create an empty template set, use the Funcs() method to register the
+        // template.FuncMap, and then parse the file as normal.
+        ts, err := template.New(name).Funcs(functions).ParseFiles("./ui/html/base.html")
         if err != nil {
             return nil, err
         }
